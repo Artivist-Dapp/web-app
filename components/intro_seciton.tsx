e1b73058b177a364f7c9e2e53c8d7c6fddea1957
Miguel Mendes <miguelangello13@gmail.com>
2022-05-24 20:14:26 +0100

    adapt top mobile

--- components/intro_seciton.tsx
+++ components/intro_seciton.tsx
@@ -8,15 +8,12 @@ interface Props {
 const IntroSection: NextPage<Props> = ({ className }) => {
   return (
     <>
-      <div className={`${className} intro-background text-on-primary h-[23rem] xl:h-[32rem] 2xl:h-[44rem]`}>
+      <div
+        className={`${className} intro-background text-on-primary h-[40rem] lg:h-[23rem] xl:h-[32rem] 2xl:h-[44rem]`}
+      >
         <div className="page-max-width relative h-full">
-            <img
-              src="intro-section-image.png"
-              alt="human-kindness"
-              className="absolute right-0 bottom-0 w-7/12  3xl:w-auto"
-            />
-          <div className="content-max-width flex flex-col justify-end pb-[6%] h-full">
-            <div className="w-[43%] 3xl:w-[43.5%] space-y-5 xl:space-y-10">
+          <div className="content-max-width flex flex-col pt-16 lg:pt-0 lg:justify-end pb-[6%] h-full">
+            <div className="lg:w-[43%] 3xl:w-[43.5%] space-y-5 xl:space-y-10">
               <div className="space-y-5 xl:space-y-10">
                 <IconDot className="w-4 aspect-square" />
                 <h4 className="text-2xl xl:text-[2rem] 2xl:text-[2.55rem] leading-tight font-alfaslabone">
@@ -31,6 +28,11 @@ const IntroSection: NextPage<Props> = ({ className }) => {
               </div>
             </div>
           </div>
+          <img
+            src="intro-section-image.png"
+            alt="human-kindness"
+            className="lg:absolute right-0 bottom-0 lg:w-7/12  3xl:w-auto"
+          />
         </div>
       </div>
     </>
