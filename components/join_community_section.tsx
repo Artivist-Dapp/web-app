c0a82db4fab9582ba155d95c5cf34e04cea8b006
Miguel Mendes <miguelangello13@gmail.com>
2022-05-25 23:50:00 +0100

    make join community section resposive

--- components/join_community_section.tsx
+++ components/join_community_section.tsx
@@ -7,25 +7,27 @@ interface Props {
 const JoinCommunitySection: NextPage<Props> = ({ className }) => {
   return (
     <>
-      <div className={`${className} join-community-background h-[50rem]`}>
+      <div
+        className={`${className} join-community-background lg:h-[27rem] xl:h-[40rem] 2xl:h-[50rem]`}
+      >
         <div className="page-max-width relative h-full">
           <div className="content-max-width flex flex-col pt-16 lg:pt-0 lg:justify-end pb-[6%] h-full">
-            <div className="space-y-10 w-[44.5%]">
-              <h4 className="font-alfaslabone text-4xl text-on-primary">
+            <div className="space-y-10 lg:w-6/12 2xl:w-[44.5%]">
+              <h4 className="font-alfaslabone text-2xl xl:text-4xl text-on-primary">
                 Join our collaborative
                 <br /> environment and have access to a decentralized world of
                 possibilities
               </h4>
-              <p className="text-3xl leading-snug text-on-primary">
+              <p className="text-xl xl:text-3xl leading-snug text-on-primary">
                 Become an amplifier for artists and organizations who strive for
                 positive social and environmental transformation. Register to
                 know more about Artivist and our community.
               </p>
-              <div className="flex space-x-16 items-center">
-                <button className="w-full px-4 py-2 bg-on-primary clickable uppercase font-bold tracking-widest rounded shadow">
+              <div className="flex flex-col lg:flex-row space-y-3 lg:space-y-0 lg:space-x-10 xl:space-x-16 items-center">
+                <button className="w-full px-4 py-2 bg-on-primary text-sm xl:text-base clickable uppercase font-bold tracking-widest rounded shadow">
                   join the community
                 </button>
-                <button className="w-full px-4 py-2 bg-on-primary clickable uppercase font-bold tracking-widest rounded shadow">
+                <button className="w-full px-4 py-2 bg-on-primary text-sm xl:text-base clickable uppercase font-bold tracking-widest rounded shadow">
                   help now!
                 </button>
               </div>
@@ -34,7 +36,7 @@ const JoinCommunitySection: NextPage<Props> = ({ className }) => {
           <img
             src="join-community-section-image.png"
             alt="human-kindness"
-            className="lg:absolute right-0 bottom-0 lg:w-7/12 -mt-40 lg:mt-0 3xl:w-auto"
+            className="lg:absolute right-0 bottom-0 w-11/12 lg:w-5/12 ml-auto 2xl:w-auto"
           />
         </div>
       </div>
