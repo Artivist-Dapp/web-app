8bff7313bad7eae4c390b1d269fef4b572546453
Miguel Mendes <miguelangello13@gmail.com>
2022-05-26 18:23:18 +0100

    remove unused components

--- components/divider.tsx
+++
@@ -1,5 +0,0 @@
-import { NextPage } from "next";
-
-const Divider: NextPage = () => <div className="w-full h-px bg-accent/70" />;
-
-export default Divider;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import BenefitSection from "../components/benefit_section";
-import Divider from "../components/divider";
 import HeroSection from "../components/hero_section";
 import HowSection from "../components/how_section";
 import IntroSection from "../components/intro_seciton";
@@ -11,41 +9,7 @@ import WhoSection from "../components/who_section";
 import WhySection from "../components/why_section";
 import { Partner } from "../types";
 
-interface Benefits {
-  title: string;
-  benefits: string;
-  imageUrl: string;
-  cta?: string;
-}
-
 const Home: NextPage = () => {
-  const benefits: Benefits[] = [
-    {
-      imageUrl: "/artist.png",
-      title: "Innovations for artist",
-      benefits: `Posssibility of becoming an active advocate for a cause through your art.
-      Get rewarded for your artistic engagements.
-      Scale your earnings from artistic royalties through smart contracts.`,
-      cta: "Explore",
-    },
-    {
-      imageUrl: "/ativist.png",
-      title: "Innovations for artivist",
-      benefits: `Becoming an active advocate of a cause by colloborating in a secure and transparent sytem.
-      Receive rewards or discounts resulting from your volume of participation.
-      Opportunity to publicize crypto funding canpaigns that promote causes you deem relevant.`,
-      cta: "Explore",
-    },
-    {
-      imageUrl: "/ngo.png",
-      title: "Innovations for NGO",
-      benefits: `Posssibility to publicize the institution and its respective cause, from its own initiative or third parties sharing cause.
-      An alternative source of funding for awareness/solidarity campaign using a system of intelligent mobilization.      
-      Introduction of NGO to the Web3 ecosystem and decentralized managementand mobiloization.`,
-      cta: "Explore",
-    },
-  ];
-
   const partners: Array<Partner> = [];
   for (let i = 0; i < 5; i++) {
     if (i === 2) {
@@ -78,15 +42,6 @@ const Home: NextPage = () => {
         <HowSection featuredArtist={featuredArtist} />
         <NearSponserSection />
         <PartnersSection partners={partners} />
-        {/* <Divider />
-          <BenefitSection benefits={benefits} />
-          <Divider />
-          <PartnersSection
-            title="Live partners"
-            partners={partners}
-            className="max-w-screen-lg mx-auto "
-          />
-          <Divider /> */}
       </div>
     </>
   );
