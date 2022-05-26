930c727342c4e2e849980e460e41fe5777e6042d
Miguel Mendes <miguelangello13@gmail.com>
2022-05-26 15:59:38 +0100

    fix design on desktop screens

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,12 +12,12 @@ const Footer: NextPage<Props> = ({ className }) => {
   return (
     <>
       <div
-        className={`${className}  bg-primary/80 pt-40 pb-10 relative text-on-primary`}
+        className={`${className}  bg-primary/80 pt-32 2xl:pt-40 pb-10 relative text-on-primary`}
       >
         <div className="page-max-width">
-          <div className="content-max-width space-y-32">
+          <div className="content-max-width space-y-20 2xl:space-y-32">
             <div className="flex flex-col lg:flex-row space-y-10 lg:space-y-0 lg:justify-between items-center">
-              <div className="space-y-40 w-5/12">
+              <div className="space-y-32 2xl:space-y-40 w-7/12 xl:w-5/12">
                 <div className="space-y-20">
                   <Logo className="text-paragraph w-80" />
                   <p className="text-2xl leading-snug font-medium">
@@ -28,7 +28,7 @@ const Footer: NextPage<Props> = ({ className }) => {
                   </p>
                 </div>
                 <div className="space-y-20">
-                  <div className="py-2 bg-on-primary text-paragraph text-center rounded absolute left-0 w-[46%] -mt-20">
+                  <div className="py-2 bg-on-primary text-paragraph lg:pl-[5%] xl:pl-[4.2%] 3xl:text-center rounded absolute left-0 w-7/12 xl:w-[46%] -mt-20">
                     <h4 className="text-3xl 2xl:text-4xl uppercase tracking-[0.5rem] font-bold">
                       Let’s keep in touch!
                     </h4>
@@ -66,7 +66,7 @@ const Footer: NextPage<Props> = ({ className }) => {
               </div>
             </div>
             <div className="flex justify-end relative text-xl font-medium">
-              <div className="flex justify-end space-x-10 relative z-[2]">
+              <div className="flex justify-end space-x-5 xl:space-x-10 relative z-[2]">
                 <div>
                   <Link href="/terms-condition">
                     <a className="clickable text-on-primary hover:text-on-primary-hover ">
